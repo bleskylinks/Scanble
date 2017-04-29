88f2baecbe6a31c2685cccceabb44d1acfead1b2
bleskylinks <bleskylinks@hotmail.com>
2017-04-29 21:19:54 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/cdhamannavar/scanble/MainActivity.java
+++ app/src/main/java/com/example/cdhamannavar/scanble/MainActivity.java
@@ -36,7 +36,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import android.os.Vibrator;
-
+//Get this update
 public class MainActivity extends Activity {
 
     private BluetoothAdapter mBluetoothAdapter;
